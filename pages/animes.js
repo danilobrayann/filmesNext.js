841b1e6bed36ceaed55ea0484af76768b6ac7391
Danilo Souza <danilobrayann@gmail.com>
2023-11-17 20:27:10 -0300

    feat: criando rotas com o netx.js

---
+++ pages/animes.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function Animes (params) {
+    
+    return(
+        <>
+        <section >
+           <img src="" alt="anime01"/>
+           <img src="" alt="anime02"/>
+           <img src="" alt="anime03"/>
+        </section>
+        </>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head"
+import styles from '../styles/Home.module.css'
+export default function Home () {
+  return(
+    <>
+    <Head>
+      <title>Animes</title>
+    </Head>
+    <h1 className={styles.cor}>hello</h1>
+    </>
+  )
+}
