356315980a78cff9a50b5112d628c80cb4e7513b
Danilo Souza <danilobrayann@gmail.com>
2023-11-17 20:55:07 -0300

    feat: implementando os link de rotas

--- pages/animes.js
+++ pages/animes.js
@@ -1,14 +1,26 @@
 import Link from "next/link"
+import Head from "next/head"
+export default function Animes() {
 
-export default function Animes (params) {
-    
-    return(
+    return (
         <>
-        <section >
-           <img src="" alt="anime01"/>
-           <img src="" alt="anime02"/>
-           <img src="" alt="anime03"/>
-        </section>
+
+            <Head>
+                <title>Animes</title>
+
+            </Head>
+            <Link href="/">
+                <h1>Home</h1>
+            </Link>
+            <Link href='/animes'>
+                <h1>Animes</h1>
+            </Link>
+
+            <section >
+                <img src="" alt="anime01" />
+                <img src="" alt="anime02" />
+                <img src="" alt="anime03" />
+            </section>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Head from "next/head"
 import styles from '../styles/Home.module.css'
-export default function Home () {
-  return(
+import Link from "next/link"
+export default function Home() {
+  return (
     <>
-    <Head>
-      <title>Animes</title>
-    </Head>
-    <h1 className={styles.cor}>hello</h1>
+      <Head>
+        <title>Animes</title>
+
+      </Head>
+      <Link href="/">
+        <h1>Home</h1>
+      </Link>
+      <Link href='/animes'>
+        <h1>Animes</h1>
+      </Link>
+      <h1 className={styles.cor}>hello</h1>
     </>
   )
 }
